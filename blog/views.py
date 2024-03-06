d1780366ed1931c561013568ef161aa3a8d505d8
Zir <143411003+Zir756@users.noreply.github.com>
2024-03-06 17:30:05 +0900

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -20,6 +20,10 @@ from .models import ToDo
 
 from .forms import ToDoForm  # ToDoフォームをインポート
 
+from .models import Note
+
+from .forms import NoteForm
+
 # Create your views here.
 
 # post_list関数  
@@ -174,3 +178,42 @@ def todo_delete(request, pk):
     todo = get_object_or_404(ToDo, pk=pk)
     todo.delete()
     return redirect('todo_list')  # Todoの一覧ページにリダイレクト
+
+@login_required   
+def note_list(request):
+    notes = Note.objects.all()
+    return render(request, 'note/note_list.html', {'notes': notes})
+
+@login_required
+def note_detail(request, pk):
+    note = Note.objects.get(pk=pk)
+    return render(request, 'note/note_detail.html', {'note': note})
+
+@login_required
+def note_create(request):
+    if request.method == 'POST':
+        form = NoteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('post_list')
+    else:
+        form = NoteForm()
+    return render(request, 'note/note_create.html', {'form': form})
+
+@login_required
+def note_update(request, pk):
+    note = Note.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = NoteForm(request.POST, instance=note)
+        if form.is_valid():
+            form.save()
+            return redirect('note_list')
+    else:
+        form = NoteForm(instance=note)
+    return render(request, 'note/note_form.html', {'form': form})
+
+@login_required
+def note_delete(request, pk):
+    note = Note.objects.get(pk=pk)
+    note.delete()
+    return redirect('note_list')
